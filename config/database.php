479e5362945045e78ef2bc523a88327cfc5cf1b7
Charmi Zala <31683538+Charmiz@users.noreply.github.com>
2023-03-26 22:09:10 +0530

    Update database.php

--- config/database.php
+++ config/database.php
@@ -6,10 +6,10 @@ $capsule = new Capsule();
 
 $capsule->addConnection([
     'driver'    => 'sqlsrv',
-    'host'      => 'localhost',
-    'database'  => 'PHP7CrudExample',
-    'username'  => 'kr',
-    'password'  => 'aise',
+    'host'      => 'php7-mssql-server.database.windows.net',
+    'database'  => 'php7-mssql-database',
+    'username'  => 'php7-mssql-server-admin',
+    'password'  => '0E6AAB31QP1147KL$',
     'charset'   => 'utf8',
     'collation' => 'utf8_unicode_ci',
     'prefix'    => '',
