30cc8c894eccb0373e3fad0f3f9b3ddc8787718c
Charmi Zala <31683538+Charmiz@users.noreply.github.com>
2023-03-26 20:17:28 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,9 @@
 use KodeBlog\Controllers\ItemsController;
 
 require './bootstrap/autoload.php';
-//print_r($_SERVER['REQUEST_URI']);
-print_r($_SERVER['SERVER_NAME']);
+
+print_r($_SERVER['REQUEST_URI']);
+
 $base_url = 'http://localhost:8000/php7sqlsrv/index.php';
 
 if (!isset($_GET['action'])) {
